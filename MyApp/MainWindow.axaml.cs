cfc4ad373bdc0e9cf17a628e65f345d6d1bd07ac
ibrahimsinanseven <ibrahim.sinan5544@icloud.com>
2023-06-01 12:56:50 +0300

    Değişiklik

---
+++ MyApp/MainWindow.axaml.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Net.Http;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace MyApp;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+
+    private void TiklA(object? sender, RoutedEventArgs e)
+    {
+        
+    }
+
+    private void button_Click(object? sender, RoutedEventArgs e)
+    {
+        
+    }
+}
